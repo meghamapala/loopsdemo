90a1e1781a47a8b4ae22f35a0b4bdf09c49285fe
Megha Mapalagama <meghamapalagama@meghas-mbp.home>
2021-03-24 12:09:03 -0400

    first commit

---
+++ src/welcome/HelloWorld.java
@@ -0,0 +1,26 @@
+package welcome;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("msg");
+		
+		for(int i =10; i<15; i++) {
+			System.out.println(i);
+			//System.out.println("*****");
+		}
+		
+		System.out.println("*****");
+		
+		int num = 20;
+		
+		while(num<=30) {
+			
+			System.out.println(num);
+			num=num-2;
+		}
+
+	}
+
+}
